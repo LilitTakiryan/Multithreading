d0dbc699089035b465187939a37ebe9080199a64
Lilit Takiryan <39703980+LilitTakiryan@users.noreply.github.com>
2021-11-19 00:27:14 +0400

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -1,26 +1,35 @@
-/*Java Program That counts sum of the numbers from 1 to 10000000 using single thread*/
+//Java Program That counts sum of the numbers from 1 to 10000000 using single thread
+
 package Sum;
 
 import java.math.BigDecimal;
 
 public class Sum implements Runnable {
+    static int number;
+
+    public Sum(int number) {
+        Sum.number = number;
+    }
+
     public static void main(String[] args) {
-        Sum sum = new Sum();
+        int number=10000000;
+        Sum sum = new Sum(number);
         Thread single = new Thread(sum);
         single.setName("Single");
         single.start();
         System.out.println(Thread.currentThread().getName());
         System.out.println("Number of active threads: " + Thread.activeCount());
     }
+
     @Override
     public void run() {
         int first = 1;
-        int last = 10000000;
+        int last = number;
         long sum;
         //n(n + 1)/2
         try {
-            sum=((long) last * (last + first)) / 2;
-            System.out.println("Sum of number between " + first + " and " + last + " is "+sum);
+            sum = ((long) last * (last + first)) / 2;
+            System.out.println("Sum of number between " + first + " and " + last + " is " + sum);
             Thread.sleep(1000);
         } catch (NumberFormatException | InterruptedException e) {
             e.getStackTrace();
