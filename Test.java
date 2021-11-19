61ba3af358ec7412b96c811af96110effbe6ceee
Lilit Takiryan <39703980+LilitTakiryan@users.noreply.github.com>
2021-11-19 15:19:01 +0400

    Create Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,5 @@
+/*Write Java Program That counts sum of the numbers from 1 to 10000000  **   MutiThread (with 2 threads)*/
 package Sum;
-
-import java.util.SortedMap;
-
 public class Test {
     public static void main(String[] args) throws InterruptedException {
         int number = 1000000;
@@ -21,3 +19,69 @@ public class Test {
             System.out.println("Sum fo numbers between 1 to " + number + " is " + (firstSum.getfSum() + secondSum.getsSum()));
     }
 }
+
+///******************************************** FirstSum.java *******************************************//
+public class FirstSum implements Runnable {
+    int fLocalNumber; // number
+    long fSum;
+    public FirstSum(int fLocalNumber) {
+        fLocalNumber = fLocalNumber / 2;
+        this.fLocalNumber = fLocalNumber;
+    }
+    public long getfSum() {
+        return fSum;
+    }
+    public void setfSum(long fSum) {
+        this.fSum = fSum;
+    }
+    @Override
+    public void run() {
+        int first = 1;
+        int last = fLocalNumber;
+
+        try {
+            fSum = ((long) last * (last + first)) / 2;
+            setfSum(fSum);
+            System.out.println("Sum of number between " + first + " and " + last + " is " + fSum);
+            Thread.sleep(5000);
+        } catch (NumberFormatException | InterruptedException e) {
+            e.getStackTrace();
+        }
+        long start = System.currentTimeMillis();
+        System.out.println("T1 Processing time: " + (System.currentTimeMillis() - start / 1000));
+        System.out.println("Thread name: " + Thread.currentThread().getName());
+    }
+}
+
+///*********************************************** SecondSum.java  ***********************************//
+public class SecondSum implements Runnable {
+    int sLocalNumber; // number
+    long sSum;
+
+    public SecondSum(int sLocalNumber) {
+        this.sLocalNumber = sLocalNumber;
+    }
+    public long getsSum() {
+        return sSum;
+    }
+    public void setsSum(long sSum) {
+        this.sSum = sSum;
+    }
+    @Override
+    public void run() {
+        int first = sLocalNumber/2;
+        int last = sLocalNumber;
+        try {
+            sSum = ((long) first * (last + (first+1)) / 2);
+            setsSum(sSum);
+            System.out.println("Sum of number between " + first + " and " + last + " is " + sSum);
+            Thread.sleep(2000);
+        } catch (NumberFormatException | InterruptedException e) {
+            e.getStackTrace();
+        }
+        long start = System.currentTimeMillis();
+        System.out.println("T2 Processing time: " + (System.currentTimeMillis() - start / 1000));
+        System.out.println("Thread name: " + Thread.currentThread().getName());
+    }
+}
+
